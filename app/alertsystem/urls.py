145bf87d9b7c9e286b074c78e664cd6131f9d3fe
Sean Takafuji <seanytak@hawaii.edu>
2018-03-16 22:14:04 -1000

    Implemented confirmation screen for missiles

--- app/alertsystem/urls.py
+++ app/alertsystem/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'alertsystem'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('foreign/', views.foreign, name='foreign')
+    path('foreign/', views.foreign, name='foreign'),
+    path('foreign/real/', views.real_foreign, name='real_foreign'),
+    path('foreign/real/sent/', views.real_foreign_sent, name='real_foreign_sent')
 ]
--- app/alertsystem/views.py
+++ app/alertsystem/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
+import random as rand
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'alertsystem/index.html')
+    context = {
+        'title': "Aloha Alert"
+    }
+    return render(request, 'alertsystem/index.html', context=context)
 
 def foreign(request):
-    return render(request, 'alertsystem/foreign.html')
+    context = {
+        'title': "Missile Threat"
+    }
+    return render(request, 'alertsystem/foreign.html', context=context)
+
+
+def real_foreign(request):
+    send_text = ["SEND", "CONFIRM", "VERIFY", "ALERT"]
+    context = {
+        'title': "Real Missile Threat",
+        'send_text': send_text[rand.randint(0, len(send_text) - 1)],
+    }
+    return render(request, 'alertsystem/real_foreign.html', context=context)
+
+
+def real_foreign_sent(request):
+    context = {
+        'title': 'Real Missile Warning Sent'
+    }
+    return render(request, 'alertsystem/sent_threat.html', context=context)
