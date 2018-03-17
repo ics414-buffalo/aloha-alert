88823d2b2420236162ab62e48f2d0422aeb6bc4f
Sean Takafuji <seanytak@hawaii.edu>
2018-03-16 18:39:19 -1000

    Added home page and link to foreign threat page

--- app/alertsystem/urls.py
+++ app/alertsystem/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = 'alertsystem'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('foreign/', views.foreign, name='foreign')
 ]
--- app/alertsystem/views.py
+++ app/alertsystem/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'alertsystem/index.html')
+
+def foreign(request):
+    return render(request, 'alertsystem/foreign.html')
